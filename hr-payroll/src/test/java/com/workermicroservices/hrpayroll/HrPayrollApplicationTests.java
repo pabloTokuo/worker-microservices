970ac67e1fe946bc05534bec2a838c1ee261f95c
pabloTokuo <contatopbo@hotmail.com>
2022-12-27 10:04:23 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/workermicroservices/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workermicroservices.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
