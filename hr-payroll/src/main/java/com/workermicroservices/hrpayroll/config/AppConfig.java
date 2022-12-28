a06d0c5d8a3c350a60d622a40b68afcb2a3075c1
pabloTokuo <contatopbo@hotmail.com>
2022-12-28 14:05:19 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/workermicroservices/hrpayroll/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.workermicroservices.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean // RestTemplate serve para comunicar um servico com o outro(payroll/worker)
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
