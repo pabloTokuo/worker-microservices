1afa33f019af6b8295c033a91808c6fe852b0ed9
pabloTokuo <contatopbo@hotmail.com>
2023-01-09 12:28:26 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/workerms/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/workerms/hrconfigserver/HrConfigServerApplication.java
@@ -1,13 +1,17 @@
 package com.workerms.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication{
 
+	//@Value("${spring.cloud.config.server.git.username}")
+	//private String userName;
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
