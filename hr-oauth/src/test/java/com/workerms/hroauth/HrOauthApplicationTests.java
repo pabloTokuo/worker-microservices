7cd832af73d5e41d14d47e64f1f9cf853f16f9a5
pabloTokuo <contatopbo@hotmail.com>
2023-01-09 14:31:02 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/workerms/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workerms.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
