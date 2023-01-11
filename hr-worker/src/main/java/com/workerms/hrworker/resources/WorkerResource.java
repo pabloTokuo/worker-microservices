f1731910aba280f5965e77d206191126db1139fd
pabloTokuo <contatopbo@hotmail.com>
2023-01-11 13:11:28 -0300

    db config server

--- hr-worker/src/main/java/com/workerms/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/workerms/hrworker/resources/WorkerResource.java
@@ -48,11 +48,11 @@ public class WorkerResource {
     public ResponseEntity<Worker> findById(@PathVariable Long id)  {
 
 
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    //    try {
+    //        Thread.sleep(3000L);
+    //    } catch (InterruptedException e) {
+    //    throw new RuntimeException(e);
+    //    }
 
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
